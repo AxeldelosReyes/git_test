ba5e85b57aac1c262e1715d74b9bc70a4f58dce3
AxeldelosReyes <44440970+AxeldelosReyes@users.noreply.github.com>
2020-10-27 16:14:44 -0600

    Add files via upload

---
+++ sale_report_terms/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Informe sales terminos',
+    'version': '13.0.0',
+    'summary': 'Agrega un apartado en compania para definir terminos del sales order',
+    'description': 'Agrega un apartado en compania para definir terminos del sales order',
+    'author': 'Xmarts',
+    'website': 'www.xmarts.com',
+    'depends': ['base','sale','product'],
+    'data': [
+        'views/res_company.xml',
+        'views/product_template.xml',
+        'reports/sale_report.xml',
+    ],
+    'installable': True,
+}
